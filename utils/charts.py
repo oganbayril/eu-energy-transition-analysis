52ce394b0f7b9200ee3655e9b023948b3578dbe1
Ogan <oganby@gmail.com>
2026-04-18 03:27:18 +0300

    Remove unnecessary imports

--- utils/charts.py
+++ utils/charts.py
@@ -1,6 +1,4 @@
-import plotly.express as px
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 
 
 # ── Colour palette ────────────────────────────────────────────────────────────
